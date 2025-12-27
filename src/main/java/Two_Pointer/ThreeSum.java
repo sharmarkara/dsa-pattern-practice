b1c1b43103ecc6656867e15ca8941ca32d851486
Rakesh <sharmarkara@gmail.com>
2025-12-27 19:19:43 +0530

    ThreeSum problem

---
+++ src/main/java/Two_Pointer/ThreeSum.java
@@ -0,0 +1,56 @@
+package Two_Pointer;
+
+import java.util.*;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
+    }
+
+    public static List<List<Integer>> threeSumBruteForce(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
+                        Collections.sort(triplet);
+                        result.add(triplet);
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(result);
+
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return result;
+
+    }
+}
